b0c4c32cf8fa1fabd3d0d808e8dabf9b641f9123
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-06-24 21:08:14 -0700

    add ref structure to global state

--- src/utils/GlobalState.js
+++ src/utils/GlobalState.js
@@ -3,12 +3,16 @@ import React, { createContext, useReducer, useContext } from "react";
 const GlobalContext = createContext();
 const { Provider } = GlobalContext;
 
+const initialState = {
+  
+};
+
 const reducer = (state, action) => {
   
 };
 
 const GlobalProvider = ({...props }) => {
-  const [state, dispatch] = useReducer(reducer, {});
+  const [state, dispatch] = useReducer(reducer, {...initialState});
 
   return <Provider value={[state, dispatch]} {...props} />;
 };
